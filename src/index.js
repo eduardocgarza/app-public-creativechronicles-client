35b9136b99d87312bf5bb41b87a04919dd8580b1
Eduardo Garza <eduardo@garza.ca>
2023-08-14 20:40:53 -0700

    Added GA4 with React.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import AppRouter from "./router/AppRouter";
 import { AppContextProvider } from "./state/AppContext";
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-RZJ36FD15E");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
